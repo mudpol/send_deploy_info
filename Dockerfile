2927ebd9356c41332daba3d108b8603df7a6aaf6
Fedorchenko Ruslan <fedorchenko.ruslan@pdffiller.team>
2019-09-05 15:24:33 +0300

    Extended log output. Fix crash when adress was unreacheble. Add console-stamp version to package.json. Add console-stamp install id Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ WORKDIR /home/node/
 COPY teamcity-push-gw-proxy/ $WORKDIR
 
 RUN npm config set package-lock false &&\
-    npm install request express
+    npm install request express console-stamp
 
 ENV PGADDRESS http://localhost:9091
 
--- teamcity-push-gw-proxy/app.js
+++ teamcity-push-gw-proxy/app.js
@@ -2,12 +2,14 @@ var send_metric = require('request'); //nodejs request module
 var express = require('express'); //nodejs express module
 var app = express();
 var port = process.env.PORT; //Set ENV port, if undefined set default value from Dokerfile
-var pgaddress = process.env.PGADDRESS // set ENV pushgateway address, if undefined set default value from Dokerfile
+var pgaddress = process.env.PGADDRESS; // set ENV pushgateway address, if undefined set default value from Dokerfile
+var logTimeStamp = require('console-stamp')(console);
 
 app.use(express.json({limit: '1mb'})); //increase json cache limit for express
 
+
 app.post('/', function(request, res){ //take post request on "/"
-  console.log(request.body)
+  console.info('Accepted: '+request.body+'\n')
   if (request.body.build.changes.length > 0) {
     Commit = request.body.build.changes[0].version;
     Comment = request.body.build.changes[0].change.comment;
@@ -25,15 +27,21 @@ for it, if array "changes" empty parse arry "teamcityProperties" for last commit
 and set Comment variable as undefined*/
 
 
-  send_metric.post({
+  send_metric
+  .post({
     headers: {'Content-Type' : 'text/plain'},
     url:     pgaddress+'/metrics/job/'+request.body.build.buildName+'/instance/'+request.body.build.projectName,
     body:    request.body.build.projectId+'{TriggeredBy="'+request.body.build.triggeredBy+'" , BuildResult="'+request.body.build.buildResult+'" , NotifyType="'+request.body.build.notifyType+'" , BuildResultDelta="'+request.body.build.buildResultDelta+'" , BuildId="'+request.body.build.buildNumber+'" , Commit="'+Commit+'" , Comment="'+Comment+'" }'+request.body.build.buildNumber+'\n',
          }
-       );
+       )
+       .on('response',function(response) {
+         console.info("Metric: "+request.body.build.projectId+'{TriggeredBy="'+request.body.build.triggeredBy+'" , BuildResult="'+request.body.build.buildResult+'" , NotifyType="'+request.body.build.notifyType+'" , BuildResultDelta="'+request.body.build.buildResultDelta+'" , BuildId="'+request.body.build.buildNumber+'" , Commit="'+Commit+'" , Comment="'+Comment+'" }'+request.body.build.buildNumber+' was send to address: '+pgaddress+'/metrics/job/'+request.body.build.buildName+'/instance/'+request.body.build.projectName);
+         console.log(response.statusCode);
+       })
+       .on('error', function(error) {
+         console.error(error.message)
+         });
 /*Send post request to prometheus pushgateway with variable from json Web Hook*/
-  console.log("Metric: "+request.body.build.projectId+'{TriggeredBy="'+request.body.build.triggeredBy+'" , BuildResult="'+request.body.build.buildResult+'" , NotifyType="'+request.body.build.notifyType+'" , BuildResultDelta="'+request.body.build.buildResultDelta+'" , BuildId="'+request.body.build.buildNumber+'" , Commit="'+Commit+'" , Comment="'+Comment+'" }'+request.body.build.buildNumber+'was sending');
-  console.log('To address: '+pgaddress+'/metrics/job/'+request.body.build.buildName+'/instance/'+request.body.build.projectName);
 
   res.sendStatus(200); // echo the result back
 
